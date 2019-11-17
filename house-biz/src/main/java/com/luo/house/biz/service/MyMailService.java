5d9c82ef9eeb1d8b85c8bead4a3b4c1103ef513d
Administrator <2084267015@qq.com>
2019-11-17 14:23:15 +0800

    st

--- house-biz/src/main/java/com/luo/house/biz/service/MyMailService.java
+++ house-biz/src/main/java/com/luo/house/biz/service/MyMailService.java
@@ -2,11 +2,16 @@ package com.luo.house.biz.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 import java.util.Properties;
 
 /**
@@ -21,6 +26,8 @@ public class MyMailService {
 
     @Value("${spring.mail.host}")
     private String mailHost;
+    @Value("${spring.mail.default-encoding}")
+    private String defaultEncoding;
 
     //必须有这个配置，否则cannot connect port 25
     @Bean
@@ -30,6 +37,7 @@ public class MyMailService {
         javaMailSender.setHost(mailHost);
         javaMailSender.setUsername(from);
         javaMailSender.setPassword(password);
+        javaMailSender.setDefaultEncoding(defaultEncoding);
 
         // 认证相关
         Properties properties = new Properties();
@@ -49,4 +57,45 @@ public class MyMailService {
         message.setText(text);
         javaMailSender().send(message);
     }
+
+    public void sendHtmlMail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = javaMailSender().createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        javaMailSender().send(message);
+    }
+
+    public void sendFileMail(String to, String subject, String text, String filePath) throws MessagingException {
+        MimeMessage message = javaMailSender().createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        //spring.io
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String filename = file.getFilename();
+        helper.addAttachment(filename, file);
+        javaMailSender().send(message);
+    }
+
+    public void sendFilesMail(String to, String subject, String text, String filePath) throws MessagingException {
+        MimeMessage message = javaMailSender().createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        //spring.io
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String filename = file.getFilename();
+        helper.addAttachment(filename, file);
+        helper.addAttachment(filename + "_test", file);
+        javaMailSender().send(message);
+    }
+
+
 }
--- house-web/src/test/java/com/luo/house/web/MyMailServiceTest.java
+++ house-web/src/test/java/com/luo/house/web/MyMailServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {MyMailService.class})
@@ -17,6 +18,28 @@ public class MyMailServiceTest {
 
     @Test
     public void testSimpleMail() {
-        myMailService.sendSimpleMail("2084267015@qq.com", "this is text mail", "text: a simple mail");
+        myMailService.sendSimpleMail("2084267015@qq.com", "this is text mail", "text: a simple mail和");
     }
+
+    @Test
+    public void testHtmlMail() throws MessagingException {
+        String content = "<html><body><p>halo world html 好的</p></body></html>";
+        myMailService.sendHtmlMail("2084267015@qq.com", "this is html mail", content);
+    }
+
+    @Test
+    public void testOneFileMail() throws MessagingException {
+        String content = "this is a mail with file";
+        String filePath = "F:\\迅雷下载\\Java深入微服务原理改造房产销售平台\\课程资料\\各种流程图.zip";
+        myMailService.sendFileMail("2084267015@qq.com", "this is  mail with file 附件", content, filePath);
+    }
+
+    @Test
+    public void testFilesMail() throws MessagingException {
+        String content = "this is a mail with file";
+        String filePath = "F:\\迅雷下载\\Java深入微服务原理改造房产销售平台\\课程资料\\各种流程图.zip";
+        myMailService.sendFilesMail("2084267015@qq.com", "this is  mail with file 附件", content, filePath);
+    }
+
+
 }
